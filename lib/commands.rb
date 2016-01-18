71a395ff4ab3da055fb218490df9855570cf1916
Paul Renvoisé <renvoisepaul@gmail.com>
2016-01-18 16:03:18 +0100

    [UPD] add logs for Standard Error instead of crash

--- lib/commands.rb
+++ lib/commands.rb
@@ -10,6 +10,9 @@ module Visjar
       @commands.each_pair do |route, klass|
         klass.run(client, slack, recast) if route == intent
       end
+    rescue StandardError => e
+      client.send_message(slack['channel'], "Sorry, something bad happened, my creators are on it already!")
+      Log.error(e)
     end
 
     def self.register(route, klass)
